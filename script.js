076f648cdc527ff217b22d3abcdeeff6d8c12729
mikedagosta <mikesen93@hotmail.com>
2026-06-13 21:36:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-  a.addEventListener('click', e=>{
-    const target=document.querySelector(a.getAttribute('href'));
-    if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth',block:'start'});}
-  });
-});
+const toggle=document.querySelector('.nav-toggle');const nav=document.querySelector('.nav');if(toggle&&nav){toggle.addEventListener('click',()=>nav.classList.toggle('open'));}
